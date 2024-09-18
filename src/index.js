092d28831814bf256c7a03db2844f50c4b298f4a
Trần Công Danh <congdanhk07@gmail.com>
2024-09-18 18:44:47 +0700

    ✨ features: config MVC, defined controller and routes

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
 const { engine } = require("express-handlebars");
-
+const route = require("./routes");
 const app = express();
 const port = 3000;
 
 // Static file
 app.use(express.static(path.join(__dirname, "public")));
 
+// Middleware form and body
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
 // Template engine
 app.engine(
   "hbs",
@@ -20,15 +27,12 @@ app.engine(
 );
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "/resources/views"));
+
 // HTTP loggers
-app.use(morgan("combined"));
-// Define routes and render template
-app.get("/", (req, res) => {
-  res.render("home");
-});
-app.get("/news", (req, res) => {
-  res.render("news");
-});
+// app.use(morgan("combined"));
+
+//Route init
+route(app);
 
 // Listening in terminal
 app.listen(port, () => {
