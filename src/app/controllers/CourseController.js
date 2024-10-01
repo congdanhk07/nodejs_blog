f2ee91ef7cdd07c60dd2c97d688ea75c7f41a532
Trần Công Danh <congdanhk07@gmail.com>
2024-10-01 10:10:04 +0700

    🎨 update: complete show UI detail course, binding data from mongodb

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -1,9 +1,13 @@
 const Course = require("../models/Course");
-const { mutipleMongooseToObject } = require("../../util/mongoose");
+const { mongooseToObject } = require("../../util/mongoose");
 class CourseController {
   //[GET] /course/:slug
-  show(req, res) {
-    res.send("COURSE DETAIL" + req.params.slug);
+  show(req, res, next) {
+    Course.findOne({ slug: req.params.slug })
+      .then((course) => {
+        res.render("courses/show", { course: mongooseToObject(course) });
+      })
+      .catch(next);
   }
 }
 
