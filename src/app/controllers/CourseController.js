b367bd4139c501e550ddb2b20b330ad5e4c4906b
Trần Công Danh <congdanhk07@gmail.com>
2024-10-01 14:08:54 +0700

    ✨ features: handle create courses, implement lib for generate slug

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -9,6 +9,20 @@ class CourseController {
       })
       .catch(next);
   }
+  //[GET] /course/create
+  create(req, res, next) {
+    res.render("courses/create");
+  }
+  //[POST] /course/store
+  store(req, res, next) {
+    const formData = req.body;
+    formData.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`;
+    const course = new Course(req.body);
+    course
+      .save()
+      .then(() => res.redirect("/"))
+      .catch(next);
+  }
 }
 
 module.exports = new CourseController();
